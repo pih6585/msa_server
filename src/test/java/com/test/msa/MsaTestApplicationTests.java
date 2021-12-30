7e9a64bc70a24b5c3412801a024930346331dc60
pih6585 <pih6585@naver.com>
2021-12-30 15:34:52 +0900

    Initial commit

---
+++ src/test/java/com/test/msa/MsaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
